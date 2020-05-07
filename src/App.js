caa1f0e8bf9d56db08c94d1d3d16e4c06f3de449
David Andersson <lejonmanen@gmail.com>
2020-05-07 10:34:45 +0200

    Added static HTML+CSS to App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <header>
+        	<h1>Tvättbjörnar 'R us</h1>
+        	<div className="cart">Kundvagn (5)</div>
+        </header>
+        <main>
+        	<div className="products">
+        		<div>
+        			<div>Otto</div>
+        			<div>Pris: 100 kr</div>
+        			<button>Lägg till i kundvagn</button>
+        		</div>
+        		<div> Lena </div>
+        		<div> Ranger Rick </div>
+        		<div> Rocket </div>
+        		<div> Otto </div>
+        		<div> Lena </div>
+        		<div> Ranger Rick </div>
+        		<div> Otto </div>
+        		<div> Lena </div>
+        		<div> Ranger Rick </div>
+        		<div> Otto </div>
+        		<div> Lena </div>
+        		<div> Ranger Rick </div>
+        	</div>
+        </main>
+        <footer>
+        	Här berättar vi om företaget, varför tvättbjörnar är bra, osv.
+        </footer>
+        </>
+    );
 }
 
 export default App;
