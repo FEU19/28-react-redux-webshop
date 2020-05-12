989ae841835977edaafbe1596b87ea3a96477225
David Andersson <lejonmanen@gmail.com>
2020-05-12 11:21:38 +0200

    Updates

---
+++ precache-manifest.4ea11191f0d7cb9f3402e7dab39efbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727dec05079c3843869c243451dc62e3",
+    "url": "/28-react-redux-webshop/index.html"
+  },
+  {
+    "revision": "e7b0ab244e140301f006",
+    "url": "/28-react-redux-webshop/static/css/main.2fb0495f.chunk.css"
+  },
+  {
+    "revision": "47e8a6d916a28343d49b",
+    "url": "/28-react-redux-webshop/static/js/2.6286ada8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/28-react-redux-webshop/static/js/2.6286ada8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b0ab244e140301f006",
+    "url": "/28-react-redux-webshop/static/js/main.65f8e199.chunk.js"
+  },
+  {
+    "revision": "2a81a5f4e8b486f9f144",
+    "url": "/28-react-redux-webshop/static/js/runtime-main.ee258de6.js"
+  }
+]);
